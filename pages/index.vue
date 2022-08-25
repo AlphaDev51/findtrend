5e901ef3ccc8c964278f837388fcff379d9dd154
Alpha Dev <alphadev51@gmail.com>
2022-08-25 18:00:00 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,383 @@
+<template>
+  <div class="bg-black text-white">
+    <!-- ==================================== -->
+    <!-- NAVIGATION /HEADER SECTION ==================================== -->
+    <!-- ==================================== -->
+    <div class="text-white font-semibold">
+      <div class="img-bg">
+        <div>
+          <div
+            class="container mx-auto py-16 lg:py-4 flex justify-around px-6 lg:px-0"
+          >
+            <!-- left navigation -->
+            <div class="flex items-center">
+              <a href="" class="flex"><img src="/findtrend1.svg" alt="" /></a>
+            </div>
+            <!-- center navigation -->
+            <div class="flex items-center space-x-6">
+              <a href="" class="hidden lg:flex">About</a>
+              <a href="" class="hidden lg:flex">How it work</a>
+              <a href="" class="hidden lg:flex">Pricing</a>
+              <a href="" class="hidden lg:flex">Solution</a>
+              <a href="" class="hidden lg:flex">Features</a>
+            </div>
+            <div class="flex items-center space-x-6">
+              <a href="" class="hidden md:flex">Login</a>
+              <a
+                href=""
+                class="text-black font-bold flex px-8 py-2 rounded-full shadow bg-white"
+                >Register</a
+              >
+            </div>
+          </div>
+        </div>
+
+        <!-- ==================================== -->
+        <!-- HERO SECTION ==================================== -->
+        <!-- ==================================== -->
+        <div
+          class="container text-center space-y-4 py-32 text-white mx-auto px-6 lg:px-0"
+        >
+          <!-- heading -->
+          <h2 class="lg:mb-8 text-5xl lg:text-6xl font-extrabold">
+            Minimize your tabs. <br />
+            Find the trends!
+          </h2>
+          <!-- paragraph -->
+          <p id="p-hero" class="mb-4 text-sm text-#b8b8b8">
+            Don’t let your computer memories consumes all of those browser tabs.
+            <br />
+            Findtrend let you gathers all of your favorite website into one
+            place.
+          </p>
+          <!-- butoon -->
+          <div class="flex items-center justify-center space-y-4 space-x-1">
+            <div class="flex">
+              <a
+                id="hero-a"
+                href=""
+                class="text-black font-bold px-10 py-3 rounded-full shadow"
+                >Get started🔥</a
+              >
+            </div>
+            <div class="flex">
+              <img id="all1" src="/all1-hero.svg" alt="" />
+            </div>
+          </div>
+        </div>
+        <div
+          class="relative ml-36 container p-10 mx-auto px-6 lg:px-0 text-center lg:flex items-center justify-center"
+        >
+          <div
+            class="hidden lg:block absolute w-40 lg:w-1/4 transform rotate-4 right-52"
+          >
+            <img class="" src="/tab1.png" alt="" />
+          </div>
+          <div
+            class="hidden lg:block absolute w-40 lg:w-1/4 transform -rotate-4 bottom-10"
+          >
+            <img class="" src="/tab2.png" alt="" />
+          </div>
+          <div
+            class="hidden lg:block absolute w-40 lg:w-1/4 transform rotate-4 left-52"
+          >
+            <img class="" src="/tab3.png" alt="" />
+          </div>
+          <div
+            class="hidden lg:block absolute w-40 lg:w-1/4 transform -rotate-4 left-10 bottom-10"
+          >
+            <img class="" src="/tab4.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- OPEN TAB SECTION ==================================== -->
+    <!-- ==================================== -->
+    <div id="open-bg" class="bg-black">
+      <div
+        class="container text-center py-28 flex flex-col items-center justify-center mx-auto px-6 lg:px-0"
+      >
+        <h1 class="mb-12 text-6xl font-bold">Open new tabs is sh*t</h1>
+        <div class="flex space-x-4">
+          <img src="/all2-tab.svg" alt="" />
+          <div class="bg-white rounded p-6 shadow">
+            <img src="/tab5.png" width="260 px" height="82px" alt="" />
+          </div>
+          <img src="/all3-tab.svg" alt="" />
+        </div>
+        <p class="mt-10 text-sm">
+          A solution for your browser tabs and don’t make your device get slower
+          over time. <br />
+          Get ease and faster to discover a trend with just one tab.
+        </p>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- long text section==================================== -->
+    <!-- ==================================== -->
+    <div id="text" class="text-black">
+      <div class="container mx-auto px-6 lg:0">
+        <h1 class="py-28 text-left font-bold text-4xl">
+          Findtrend helps you to increase your productivity and reduce your
+          computer's memory load,
+          <span id="text-col"
+            >an application that can fulfill your daily browsing needs.</span
+          >
+        </h1>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- FINDTREND  MAKE==================================== -->
+    <!-- ==================================== -->
+    <div id="findmake" class="text-black lg:py-0 py-56">
+      <div
+        class="container py-48 flex flex-col items-center justify-center mx-auto px-6 lg:0"
+      >
+        <h2 class="font-bold text-3xl mb-12">
+          Findtrend make +1000 Startup grow
+        </h2>
+        <div id="Findsection">
+          <img src="/Brands.png" width="600" height="700" alt="" />
+        </div>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- ALL PLATEFORM CONNECT  ==================================== -->
+    <!-- ==================================== -->
+    <div id="plate-connect" class="text-black">
+      <div
+        class="container py-36 flex flex-col items-center justify-center mx-auto px-6 lg:0"
+      >
+        <h2 class="mb-5 font-bold text-2xl lg:text-3xl">
+          All platform connect to Findtrend
+        </h2>
+        <div
+          class="mb-5 grid grid-cols-5 gap-4 justify-center items-center text-center lg:w-1/2"
+        >
+          <img src="/Group 33.png " class="" alt="" />
+          <img src="/Group 34.png " class="" alt="" />
+          <img src="/Group 35.png " class="" alt="" />
+          <img src="/Group 36.png " class="" alt="" />
+          <img src="/Group 39.png " class="" alt="" />
+        </div>
+        <div class="w-full lg:w-1/3 h-full mb-5">
+          <img src="/tweet 1.png" class="mb-8 " alt="" />
+          <img src="/tweet 2.png" class="mb-8 " alt="" />
+          <img src="/tweet 3.png" class="mb-8 " alt="" />
+        </div>
+        <div class="flex items-center justify-center py-8">
+          <a
+            href=""
+            class="py-3 px-12 bg-black font-bold rounded-full text-white"
+            >Join Now</a
+          >
+        </div>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- PRICING SECTION ==================================== -->
+    <!-- ==================================== -->
+    <div id="pricing" class=" text-white ">
+      <div
+        class="container py-8 flex flex-col items-center justify-center space-y-10 mx-auto px-6 lg:0"
+      >
+        <h1 class="text-5xl mb-5 font-bold">Get your best deal</h1>
+        <div class="flex items-center justify-center space-x-4">
+          <h3 class="font-semibold">Monthly</h3>
+          <img src="/button.svg" alt="" />
+          <h4 class="text-gray-200 text-lg opacity-50">yearly</h4>
+        </div>
+        <div
+          class="lg:w-full container space-y-8 lg:space-y-0 lg:flex justify-center items-center lg:space-x-6"
+        >
+          <article
+            class="bg-white p-8 py-2 text-left rounded-lg shadow space-y-4 w-full text-black ha"
+          >
+            <h2 class="text-4xl font-semibold mb-1">Personnal</h2>
+            <p class="mb-1 border-b border-black w-full pb-2">
+              Special first packet for all
+            </p>
+            <h1 class="text-5xl mb-6 font-bold mt-10">
+              $8 <span class="text-sm font-normal">/month</span>
+            </h1>
+            <ul class="mb-8">
+              <li class="mb-4">Up to 5 page each group</li>
+              <li class="mb-4">Up to 10 group page</li>
+              <li class="mb-4">5 Days group page saved</li>
+            </ul>
+            <div class="flex items-center justify-center text-center py-36 lg:py-40">
+              <a
+                href=""
+                class="py-3 px-12 bg-black font-bold btn rounded-full w-full text-center"
+                >Start Free Trial</a
+              >
+            </div>
+          </article>
+          <article
+            id="regular"
+            class="p-8 py-2 text-left rounded-lg shadow space-y-4 w-full text-black ha"
+          >
+            <h2 class="text-4xl font-semibold mb-1">Regular</h2>
+            <p class="mb-1 border-b border-black w-full pb-2">
+              Recommended for personal pro
+            </p>
+            <h1 class="text-5xl mb-6 font-bold mt-10">
+              $20 <span class="text-sm font-normal">/month</span>
+            </h1>
+            <ul class="mb-24">
+              <li class="mb-4">Up to 15 page each group</li>
+              <li class="mb-4">Download page up to 20 page</li>
+              <li class="mb-4">Up to 10 group page</li>
+              <li class="mb-4">15 Days group page saved</li>
+            </ul>
+            <div class="flex items-center justify-center py-20 lg:py-32">
+              <a
+                href=""
+                class="py-3 px-12 bg-black font-bold rounded-full text-white w-full text-center"
+                >Start Free Trial</a
+              >
+            </div>
+          </article>
+          <article
+            class="bg-white p-8 py-2 text-left rounded-lg shadow space-y-4 w-full text-black ha"
+          >
+            <h2 class="text-4xl font-semibold mb-1">Premium</h2>
+            <p class="mb-1 border-b border-black w-full pb-2">
+              Packet for Startup & Company
+            </p>
+            <h1 class="text-5xl mb-6 mt-10 font-bold">
+              $48 <span class="text-sm font-normal">/month</span>
+            </h1>
+            <ul class="mb-8">
+              <li class="mb-4">Unlimited group pages</li>
+              <li class="mb-4">Unlimited download page</li>
+              <li class="mb-4">Unlimited page each group</li>
+              <li class="mb-4">Customize sorting group pages</li>
+              <li class="mb-4">Customize group page name</li>
+              <li class="mb-4">30 Days group page saved</li>
+            </ul>
+            <div class="flex items-center justify-center lg:py-6">
+              <a
+                href=""
+                class="py-3 px-12 bg-black font-bold btn rounded-full w-full text-center"
+                >Start Free Trial</a
+              >
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- CONTACT SECTION ==================================== -->
+    <!-- ==================================== -->
+    <div id="contact" class="text-black py-14">
+      <div class="container mx-auto px-6 lg:px-0">
+        <h1 class="text-center text-5xl lg:text-6xl font-bold mb-4">
+          Join us on email for
+        </h1>
+        <h2
+          id="h-co"
+          class="text-center bg-black text-5xl lg:text-6xl font-bold mb-6 h-24 text-transparent bg-clip-text opacity-40"
+        >
+          more trending topics
+        </h2>
+        <div class="flex items-center justify-center py-8">
+          <a
+            href=""
+            class="py-3 px-12 bg-black font-bold rounded-full text-white"
+            >Join Now</a
+          >
+        </div>
+      </div>
+    </div>
+    <!-- ==================================== -->
+    <!-- FOOTER SECTION ==================================== -->
+    <!-- ==================================== -->
+    <div id="footfind" class=" text-black bg-white py-14">
+      <div
+        class="container mx-auto text-center lg:flex justify-between items-center space-y-4 lg:space-y-0 px-6 lg:px-0"
+      >
+        <div class="   flex items-center justify-center">
+          <a href=""><img src="/footfind.svg" alt="" /></a>
+        </div>
+        <div class="space-x-6 flex items-center justify-center">
+          <a href="" class="font-semibold text-align">Privacy Policy</a>
+          <a href="" class="font-semibold text-align"> Terms and Conditions</a>
+          <a href="" class="font-semibold text-align"> Contact Us</a>
+          <a href="" class="font-semibold text-align">Careers</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.img-bg {
+  display: block;
+  height: 800px;
+  margin-left: 0;
+  background: url('/website.png') center no-repeat border-box;
+}
+
+#hero-a {
+  background: #a8ff35;
+}
+
+#all1 {
+  width: 99px;
+  height: 70px;
+}
+
+
+#open-bg {
+  width: 100%;
+  height: 500px;
+  background: #a8ff35;
+}
+
+#text {
+  height: 100px;
+  background: #fff;
+}
+
+#text-col {
+  color: #adadad;
+}
+
+#findmake {
+  max-height: 50rem;
+  background: #fff;
+}
+
+#plate-connect {
+  max-height: 115rem;
+  background: #fafafa;
+}
+
+#pricing {
+  max-height: 130rem;
+}
+
+#regular {
+  background: #a8ff35;
+}
+
+#contact {
+  height: 400px;
+  background: #a8ff35;
+}
+
+#footfind {
+  height: 20px;
+}
+
+
+.btn {
+  background: #a8ff35;
+}
+
+.ha {
+  height: 35rem;
+}
+</style>
